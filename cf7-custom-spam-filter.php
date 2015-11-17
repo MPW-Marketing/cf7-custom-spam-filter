1042bb941ee8097b0f6c77e08525ba72b4f28ca5
Damon Maldonado <damon@mpwmarketing.com>
2015-11-17 13:28:01 -0500

    working version 1

--- cf7-custom-spam-filter.php
+++ cf7-custom-spam-filter.php
@@ -8,26 +8,28 @@ Author: DMM
 Author URI:
 License: GPL2
 */
-function strposa($haystack, $needle, $offset=0) {
-    if(!is_array($needle)) $needle = array($needle);
-    foreach($needle as $query) {
-        if(strpos($haystack, $query, $offset) !== false) return true; // stop on first true result
-    }
-    return false;
-}
 
-		$spam_list = array('free website', 'increase visitors', 'website marketing', 'seo', 'web site design', 'website design', 'online marketing', 'targeted traffic', 'web marketing', 'our email list', 'remove, as the subject line', 'remove within the subject line', 'my database');
+add_filter( 'wpcf7_validate_textarea*', 'custom_textarea_spam_validation_filter', 20, 2 );
+add_filter( 'wpcf7_validate_textarea', 'custom_textarea_spam_validation_filter', 20, 2 );
+
+
+
 
 function custom_textarea_spam_validation_filter( $result, $tag ) {
+			$spam_list = array('free website', 'increase visitors', 'website marketing', 'seo', 'web site design', 'website design', 'online marketing', 'targeted traffic', 'web marketing', 'our email list', 'remove, as the subject line', 'remove within the subject line', 'my database', 'business directories' , 'get listed');
 	$tag = new WPCF7_Shortcode( $tag );
-	if ( 'your-message-t' == $tag->name ) {
-		if ( isset($_POST['your-message-t'] ) ) {
-			$your_message = sanitize_text_field( $_POST['your-message-t'] );
-		}
-
-	if (strposa($your_message, $spam_list)) {
-		$result->invalidate( $tag, "Please check the content of your message" );
+	if ( 'your-message' == $tag->name ) {
+	$your_message = isset( $_POST['your-message'] ) ? trim( $_POST['your-message'] ) : '';
+	$your_message = strtolower($your_message);
+	$is_spam = false;
+    foreach($spam_list as $query) {
+	if (strpos($your_message, $query) !== false ) {
+		$is_spam = true;
 	}
+}
+if ($is_spam) {
+			$result->invalidate( $tag, "Please check the content of your message" );
+}
 }
 return $result;
 }
