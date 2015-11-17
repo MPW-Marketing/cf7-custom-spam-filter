571e55af49d36dc7b84dcdbd224f59ba22369d55
Damon Maldonado <damon@mpwmarketing.com>
2015-11-17 09:16:59 -0500

    initial

---
+++ cf7-custom-spam-filter.php
@@ -0,0 +1,33 @@
+<?php
+/*
+Plugin Name: Contact Form 7 custom spam filter additions
+Plugin URI:
+Description: fitler text content for specific terms
+Version: 0.1
+Author: DMM
+Author URI:
+License: GPL2
+*/
+function strposa($haystack, $needle, $offset=0) {
+    if(!is_array($needle)) $needle = array($needle);
+    foreach($needle as $query) {
+        if(strpos($haystack, $query, $offset) !== false) return true; // stop on first true result
+    }
+    return false;
+}
+
+		$spam_list = array('free website', 'increase visitors', 'website marketing', 'seo', 'web site design', 'website design', 'online marketing', 'targeted traffic', 'web marketing', 'our email list', 'remove, as the subject line', 'remove within the subject line', 'my database');
+
+function custom_textarea_spam_validation_filter( $result, $tag ) {
+	$tag = new WPCF7_Shortcode( $tag );
+	if ( 'your-message-t' == $tag->name ) {
+		$your_email = if ( isset($_POST['your-message-t'] ) ) {
+			$your_email = sanitize_text_field( $_POST['your-message-t'] );
+		}
+
+	if (strposa($your_email, $spam_list)) {
+		$result->invalidate( $tag, "Please check the content of your message" );
+	}
+}
+return $result;
+}
